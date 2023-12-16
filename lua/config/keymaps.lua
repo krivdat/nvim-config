2df94d1ee21a16410e87e8e15cf6b5cf558f48f5
Tomas Krivda <krivdat@gmail.com>
2023-12-16 17:37:32 +0100

    more changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,21 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Directory Navigation
-keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>", opts)
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+
+-- Pane Navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Window Management 
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
+keymap.set("n", "<leader>sh", ":split<CR>", opts)
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts)
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
