aa158974626accd28b20067ee104bb7205f07028
Tomas Krivda <krivdat@gmail.com>
2023-12-17 23:45:25 +0100

    added lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,6 +16,11 @@ keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
 keymap.set("n", "<leader>sh", ":split<CR>", opts)
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts)
 
+-- Indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
 -- Comments
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+
