d4bbc3165d3b25e6c24dba417eccc4ba9317a452
Tomas Krivda <krivdat@gmail.com>
2024-01-30 23:55:27 +0100

    added key bindings to markdown preview & set dark theme

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -7,4 +7,9 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
+  init = function ()
+    vim.g.mkdp_theme = "dark"
+    vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", {})
+    vim.keymap.set("n", "<leader>mc", ":MarkdownPreviewStop<CR>", {})
+  end
   }
